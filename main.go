78277968033e1f5f9ee4c0dc079cd5976cba5176
k1LoW <k1lowxb@gmail.com>
2023-09-11 13:56:47 +0900

    Append mixedcaps.Analyzer

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"github.com/k1LoW/gostyle/analyzer/effective/ifacenames"
+	"github.com/k1LoW/gostyle/analyzer/effective/mixedcaps"
 	"golang.org/x/tools/go/analysis/unitchecker"
 )
 
 func main() {
 	unitchecker.Main(
 		ifacenames.Analyzer,
+		mixedcaps.Analyzer,
 	)
 }
